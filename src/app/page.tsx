6920b3cbc823c6592218c37f0764a65f58c78537
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-11-03 04:33:34 +0530

    Nextjs + shadcn + dark mode setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { ModeToggle } from "@/components/theme/modeToggle";
+import { Button } from "@/components/ui/button";
+
+function HomePage() {
+  return (
+    <div>
+      <p>HomePage</p>
+      <Button>Click Me</Button>
+      <ModeToggle />
+    </div>
+  );
+}
+
+export default HomePage;
